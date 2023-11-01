731264e5d0dba8f9e5742b7afe9b7c9af012951a
lyx <915227825@qq.com>
2023-11-01 11:14:52 +0800

    keke2.0

--- util/buildMsg.js
+++ util/buildMsg.js
@@ -180,7 +180,7 @@ const packMsg = async(data) => {
   var togetherDays = "和小狗在一起:" + data.togetherDays.value + "\n";
   var birthday = "距离珂珂的下一个生日:" + data.birthday.value + "\n";
   // var leoSaying = "小狗一直很想你呀！！！"
-  var leoSaying = "小狗一直很想你呀！！！" + "\n" + "珂珂，糕点好次吗?";
+  var leoSaying = "小狗一直很想你呀！！！" + "\n" + "口罩头盔珂你好！";
 
   var res = title + userName + nowDate + city + weather + real + est + wind + tips + saylove + togetherDays + birthday + leoSaying
   return res
